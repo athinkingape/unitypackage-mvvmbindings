59420b2f8d68d848a73f8012c2156ca3339ee986
Tymur Koshel <timurkoshel@gmail.com>
2024-05-13 14:04:35 -0700

    Button to action binding

--- Runtime/ButtonBinding.cs
+++ Runtime/ButtonBinding.cs
@@ -1,3 +1,4 @@
+using System;
 using MVVM.Bindings.Base;
 using MVVM.Commands;
 using UnityEngine.UI;
@@ -8,6 +9,7 @@ namespace MVVM.Bindings
     {
         private readonly Button _button;
         private readonly ICommand _command;
+        private readonly Action _action;
         
         public ButtonBinding(Button button, ICommand command)
         {
@@ -15,19 +17,25 @@ namespace MVVM.Bindings
             _command = command;
         }
 
+        public ButtonBinding(Button button, Action action)
+        {
+            _button = button;
+            _action = action;
+        }
+
         public void OnEnable()
         {
-            _button.onClick.AddListener(_command.Execute);
+            _button.onClick.AddListener(_command == null ? _action.Invoke : _command.Execute);
         }
 
         public void OnDisable()
         {
-            _button.onClick.RemoveListener(_command.Execute);
+            _button.onClick.RemoveListener(_command == null ? _action.Invoke : _command.Execute);
         }
 
         public void OnDestroy()
         {
-            _button.onClick.RemoveListener(_command.Execute);
+            _button.onClick.RemoveListener(_command == null ? _action.Invoke : _command.Execute);
         }
     }
 }
