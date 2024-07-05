df6e8442dc35a55c9f20470e1b0f21275e84d8d7
Tymur Koshel <timurkoshel@gmail.com>
2024-07-05 12:57:43 -0700

    better ListView

--- Runtime/ListBinding.cs
+++ Runtime/ListBinding.cs
@@ -15,112 +15,67 @@ namespace MVVM.Bindings
         private readonly Transform _container;
         private readonly TView _prefab;
         
-        private HashSet<TView> _instantiatedViews;
-        private readonly List<TView> _viewsToDestroy = new(); 
+        private HashSet<TView> _instantiatedViews; 
 
         public ListBinding(IEnumerable<TView> instantiatedViews, IObservableValue<IEnumerable<TViewModel>> observableValue) : base(observableValue)
         {
             _instantiatedViews = new HashSet<TView>(instantiatedViews);
+            _container = instantiatedViews.First().transform;
         }
         
         public ListBinding(IEnumerable<TView> instantiatedViews, IEnumerable<TViewModel> value) : base(value)
         {
             _instantiatedViews = new HashSet<TView>(instantiatedViews);
+            _container = instantiatedViews.First().transform;
         }
 
         public ListBinding(Transform container, TView prefab, IObservableValue<IEnumerable<TViewModel>> observableValue) : base(
             observableValue)
         {
+            _instantiatedViews = new HashSet<TView>();
             _container = container;
             _prefab = prefab;
         }
 
         protected override void OnUpdate(IEnumerable<TViewModel> value)
         {
-            if (_instantiatedViews == null)
-            {
-                InstantiateViews(value);
-            }
-            else
-            {
-                ManageInstantiatedViews(value);
-            }
+            ManageInstantiatedViews(value);
         }
 
-        private void InstantiateViews(IEnumerable<TViewModel> value)
-        {
-            _instantiatedViews = new HashSet<TView>();
-            
-            var max = value.Count();
-            var viewModelsEnumerator = value.GetEnumerator();
-
-            while (max > 0)
-            {
-                viewModelsEnumerator.MoveNext();
-
-                var viewModel = viewModelsEnumerator.Current;
-                var view = Instantiate();
-                
-                view.Setup(viewModel);
-
-                max--;
-            }
-            
-            viewModelsEnumerator.Dispose();
-        }
-
-        private TView Instantiate()
-        {
-            var view = Object.Instantiate(_prefab, _container);
-            _instantiatedViews.Add(view);
-            return view;
-        }
+        private TView Instantiate() => Object.Instantiate(_prefab, _container);
 
         private void ManageInstantiatedViews(IEnumerable<TViewModel> value)
         {
             var max = Mathf.Max(_instantiatedViews.Count, value.Count());
 
-            var viewsEnumerator = _instantiatedViews.GetEnumerator();
+            var viewsEnumerator = _instantiatedViews.ToList().GetEnumerator();
             var viewModelsEnumerator = value.GetEnumerator();
 
             while (max > 0)
             {
-                viewsEnumerator.MoveNext();
-                viewModelsEnumerator.MoveNext();
-                
-                var view = viewsEnumerator.Current;
-                var viewModel = viewModelsEnumerator.Current;
+                var view = viewsEnumerator.MoveNext() ? viewsEnumerator.Current : null;
+                var viewModel = viewModelsEnumerator.MoveNext() ? viewModelsEnumerator.Current : null;
 
                 if (view == null)
                 {
                     view = Instantiate();
+                    _instantiatedViews.Add(view);
                 }
 
                 if (viewModel == null)
                 {
-                    _viewsToDestroy.Add(view);
+                    view.gameObject.SetActive(false);
                 }
-                
-                view.Setup(viewModel);
+                else
+                {
+                    view.Setup(viewModel);
+                }
 
                 max--;
             }
             
             viewsEnumerator.Dispose();
             viewModelsEnumerator.Dispose();
-
-            DestroyUnusedViews();
-        }
-
-        private void DestroyUnusedViews()
-        {
-            foreach (var view in _viewsToDestroy)
-            {
-                _instantiatedViews.Remove(view);
-                Object.Destroy(view);
-            }
-            
-            _viewsToDestroy.Clear();
         }
     }
 }
