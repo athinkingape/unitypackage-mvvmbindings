120202a12e4a6b6cf0a03fd7245808dfb2f794d9
Geoff <ggollmer@athinkingape.com>
2025-02-19 18:07:43 -0800

    Add inverted to interactable binding.

--- Runtime/InteractableBinding.cs
+++ Runtime/InteractableBinding.cs
@@ -5,20 +5,23 @@ using UnityEngine.UI;
 namespace MVVM.Bindings {
     public class InteractableBinding : BaseValueBinding<bool> {
         private readonly Selectable _selectable;
+        private readonly bool _isInverted;
 
-        public InteractableBinding(IObservableValue<bool> observableValue, Selectable selectable) : base(observableValue)
+        public InteractableBinding(IObservableValue<bool> observableValue, Selectable selectable, bool isInverted = false) : base(observableValue)
         {
             _selectable = selectable;
+            _isInverted = isInverted;
         }
 
-        public InteractableBinding(bool value, Selectable selectable):base(value) {
+        public InteractableBinding(bool value, Selectable selectable, bool isInverted = false):base(value) {
             _selectable = selectable;
             _selectable.interactable = value;
+            _isInverted = isInverted;
         }
 
         protected override void OnUpdate(bool value)
         {
-            _selectable.interactable = value;
+            _selectable.interactable = _isInverted ? !value : value;
         }
     }
 }
